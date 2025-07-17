20533d611650f619208ab43bc46e6c46dbd3aec2
Ismar Costabile <ism.costabile@stud.uniroma3.it>
2025-07-17 14:19:01 +0200

    Initial commit: Spring Boot epaper planner project setup

---
+++ src/main/java/com/ismarc/epaper_planner/epaper_planner/EpaperPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.ismarc.epaper_planner.epaper_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpaperPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpaperPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ismarc/epaper_planner/epaper_planner/EpaperPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismarc.epaper_planner.epaper_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpaperPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
