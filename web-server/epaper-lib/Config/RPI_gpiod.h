cdd49038921595eef4a5e5009ba1f603ecfbfdbf
Ismar Costabile <ism.costabile@stud.uniroma3.it>
2025-07-26 23:37:03 +0200

    webserver fixes

--- web-server/epaper-lib/Config/RPI_gpiod.h
+++ web-server/epaper-lib/Config/RPI_gpiod.h
@@ -26,7 +26,6 @@
 # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#
 ************************D******************************************************/
 #ifndef __GPIOD_
 #define __GPIOD_
